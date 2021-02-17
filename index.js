4e30adf37764af53d13110ea6117268faaafa922
Antsis0077 <antsis0077@users.noreply.github.com>
2021-02-17 17:54:56 +1030

    UXUIHW20_17.2.21_554P

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("navbar navbar-expand-sm bg-dark navbar-darkr");
-var sticky = header.offsetTop;
+// var header = document.getElementById("navbar navbar-expand-sm bg-dark navbar-darkr");
+// var sticky = header.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
   } else {
-    header.classList.remove("sticky");
+    mybutton.style.display = "none";
   }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
